dfaf20e0cb20a6220815e985086761dd1c303b87
BlueSkyer-25 <775154084@qq.com>
2017-12-08 22:31:44 +0800

    Initial Commit

---
+++ JDLProjectTools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JDLProjectTools
+//
+//  Created by 胜炫电子 on 2017/12/8.
+//  Copyright © 2017年 BlueSkyer-25. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JDLProjectTools/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JDLProjectTools
+//
+//  Created by 胜炫电子 on 2017/12/8.
+//  Copyright © 2017年 BlueSkyer-25. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
